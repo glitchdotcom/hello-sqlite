e0832e313dcb3fa560711cafcc149066493f729b
Glitch (longing-eel) <none>
2019-11-05 19:29:13 +0000

    🚶🗻 Updated with Glitch

--- README.md
+++ README.md
@@ -1,31 +1,25 @@
-Welcome to Glitch
-=================
+# hello-sqlite
 
-Click `Show` in the header to see your app live. Updates to your code will instantly deploy and update live.
+A starter with has a database
 
-**Glitch** is the friendly community where you'll build the app of your dreams. Glitch lets you instantly create, remix, edit, and host an app, bot or site, and you can invite collaborators or helpers to simultaneously edit code with you.
-
-Find out more [about Glitch](https://glitch.com/about).
-
-
-Your Project
-------------
+- this app uses sqlite but you can power your apps with [a number of other storage options](https://glitch.com/storage)
+- `sqlite.db` is created and put into the `.data` folder, a hidden directory whose contents aren’t copied when a project is remixed. you can see the contents of `.data` in the console under "Logs"
+- to save to the database, remix this app!
 
 On the front-end,
-- edit `public/client.js`, `public/style.css` and `views/index.html`
+
+- edit `views/index.html`,  `public/style.css`, and `public/client.js`
 - drag in `assets`, like images or music, to add them to your project
 
 On the back-end,
+
 - your app starts at `server.js`
 - add frameworks and packages in `package.json`
 - safely store app secrets in `.env` (nobody can see this but you and people you invite)
 
-This app has a database!
-- this app uses sqlite but you can power your apps with [a number of other storage options](https://glitch.com/storage)
-- `sqlite.db` is created and put into the `.data` folder, a hidden directory whose contents aren’t copied when a project is remixed. you can see the contents of `.data` in the console under "Logs"
+Click `Show` in the header to see your app live. Updates to your code will instantly deploy.
 
 
-Made by [Glitch](https://glitch.com/)
--------------------
+## Made by [Glitch](https://glitch.com/)
 
-\ ゜o゜)ノ
+\ ゜ o ゜)ノ
--- public/client.js
+++ public/client.js
@@ -3,42 +3,49 @@
 
 console.log("hello world :o");
 
-let dreams = [];
+const dreams = [];
 
 // define variables that reference elements on our page
-const dreamsList = document.getElementById("dreams");
 const dreamsForm = document.forms[0];
 const dreamInput = dreamsForm.elements["dream"];
-
-// a helper function to call when our request for dreams is done
-const getDreamsListener = function() {
-  // parse our response to convert to JSON
-  dreams = JSON.parse(this.responseText);
-
-  // iterate through every dream and add it to our page
-  dreams.forEach(function(row) {
-    appendNewDream(row.dream);
-  });
-};
+const dreamsList = document.getElementById("dreams");
+const dreamsClearBtn = document.getElementById('clear-dreams');
 
 // request the dreams from our app's sqlite database
-const dreamRequest = new XMLHttpRequest();
-dreamRequest.onload = getDreamsListener;
-dreamRequest.open("get", "/getDreams");
-dreamRequest.send();
+fetch("/getDreams", {
+  method: "GET"
+})
+  .then(res => res.json())
+  .then(response => {
+    response.forEach(row => {
+      appendNewDream(row.dream);
+    });
+  });
 
 // a helper function that creates a list item for a given dream
-const appendNewDream = function(dream) {
+const appendNewDream = (dream) => {
   const newListItem = document.createElement("li");
   newListItem.innerHTML = dream;
   dreamsList.appendChild(newListItem);
 };
 
 // listen for the form to be submitted and add a new dream when it is
-dreamsForm.onsubmit = function(event) {
+dreamsForm.onsubmit = (event) => {
   // stop our form submission from refreshing the page
   event.preventDefault();
 
+  // add the dream to the database - NOTE: to actually add to the database, remix this app and uncomment lines 62-74 in server.js
+  const data = { dream: dreamInput.value };
+
+  fetch("/addDream", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: { "Content-Type": "application/json" }
+  })
+    .then(res => res.json())
+    .then(response => {
+      console.log(JSON.stringify(response));
+    });
   // get dream value and add it to the list
   dreams.push(dreamInput.value);
   appendNewDream(dreamInput.value);
@@ -47,3 +54,14 @@ dreamsForm.onsubmit = function(event) {
   dreamInput.value = "";
   dreamInput.focus();
 };
+
+const clearDreams = (event) => {
+  fetch("/clearDreams", {
+    method: "GET"
+  })
+  .then(res => res.json())
+  .then(response => {
+    console.log('cleared dreams');
+    dreamsList.innerHTML = '';
+  })
+}
